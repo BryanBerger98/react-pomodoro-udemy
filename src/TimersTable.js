6d724441f77ed53d2126ed487b1003d726911d0c
Bryan BERGER <contact@weberger.net>
2022-10-22 18:38:47 +0200

    update contexts

--- src/TimersTable.js
+++ src/TimersTable.js
@@ -8,32 +8,35 @@ function TimersTable(props) {
 	const { tasksData, removeTask } = useContext(TasksContext);
 
 	return (
-		<table className={ style['timers-table'] }>
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>Task</th>
-						<th>Description</th>
-						<th>Time</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						tasksData.tasks.map((task, index) => (
-							<tr key={ task.date.getMilliseconds() }>
-								<td>{ task.date.toLocaleDateString() } at { task.date.toLocaleTimeString() }</td>
-								<td>{ task.title }</td>
-								<td>{ task.description }</td>
-								<td><ClockDisplay time={ task.time } /></td>
-								<td>
-									<button onClick={ () => removeTask(index) }>Delete</button>
-								</td>
-							</tr>
-						))
-					}
-				</tbody>
+		<>
+			<h3>{tasksData.count} Task{tasksData.count > 1 ? 's' : ''} registered</h3>
+			<table className={ style['timers-table'] }>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Task</th>
+							<th>Description</th>
+							<th>Time</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							tasksData.tasks.map((task, index) => (
+								<tr key={ task.date.getMilliseconds() }>
+									<td>{ task.date.toLocaleDateString() } at { task.date.toLocaleTimeString() }</td>
+									<td>{ task.title }</td>
+									<td>{ task.description }</td>
+									<td><ClockDisplay time={ task.time } /></td>
+									<td>
+										<button onClick={ () => removeTask(index) }>Delete</button>
+									</td>
+								</tr>
+							))
+						}
+					</tbody>
 			</table>
+		</>
 	);
 }
 
