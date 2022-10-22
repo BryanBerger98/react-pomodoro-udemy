65ddbc699d68d2d1148d9d964e7a90a81637e1c9
Bryan BERGER <contact@weberger.net>
2022-10-22 18:43:26 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import Timer from './Timer';
-import TimersTable from './TimersTable';
+import Timer from "./Timer";
+// import TimersTable from "./TimersTable";
 import style from './App.module.css';
-import { useContext, useState } from 'react';
-import useTimeParser from './hooks/useTimeParser';
-import { ThemeContext } from './contexts/Theme';
+import { lazy, Suspense, useContext } from "react";
+import useTimeParser from "./hooks/useTimeParser";
+import { ThemeContext } from "./contexts/Theme";
+
+const TimersTable = lazy(() => import('./TimersTable'));
 
 function App() {
 
-	const { theme, toggleTheme } = useContext(ThemeContext);
+	const {theme, toggleTheme} = useContext(ThemeContext);
 
 	const { parseSecondsToHMS } = useTimeParser();
 
 	const displayTimerDetails = (timer) => {
 		alert(`${timer.date.toLocaleDateString()} at ${timer.date.toLocaleTimeString()} \n${parseSecondsToHMS(timer.time)}`);
-	}
+	};
 
 	return (
-		<div className={ `${style.container} ${style[theme]}` }>
-			<h1 className={ style['main-title'] }>Pomodoro Timer</h1>
-			<button onClick={ toggleTheme }>Toggle to { theme === 'light' ? 'dark' : 'light' }</button>
+		<div className={`${style.container} ${style[theme]}`}>
+			<h1 className={style['main-title']}>Pomodoro Timer</h1>
+			<button onClick={ toggleTheme }>Toggle to {theme === 'light' ? 'dark' : 'light'}</button>
 			<Timer />
-			<TimersTable onDisplayTimerDetails={ displayTimerDetails } />
+			<Suspense fallback={'Loading...'}>
+				<TimersTable onDisplayTimerDetails={ displayTimerDetails } />
+			</Suspense>
 		</div>
 	);
+
 }
 
 export default App;
